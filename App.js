341dd4008f0897f26ac9cb12a8b78b8c621922d8
frontendprof <https://github.com/frontendprof>
2021-03-31 23:51:36 +0500

    hoc

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 
+import Layout from "./src/components/Layout";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       App Builder
+      <Layout>
+          <h2>Testing</h2>
+      </Layout>
     </div>
   );
 }
--- index.js
+++ index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+
+
+
+
+
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
