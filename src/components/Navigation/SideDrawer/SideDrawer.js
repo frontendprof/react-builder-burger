4c43eb4ea1627a3a230957c85bdcfa5ad4b938f1
frontendprof <https://github.com/frontendprof>
2021-04-05 23:38:12 +0500

    reusing the backdrop

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,21 +2,32 @@
 import React from 'react'
 import Logo from '../../Logo/Logo'
 import NavigationItems from '../NavigationItems/NavigationItems';
+import Backdrop from "../../UI/Backdrop/Backdrop";
 
 import classes from "./SideDrawer.module.css";
 
+import Aux from "../../../hoc/Aux";
+
 const SideDrawer = (props) => {
+    let attachedClass=[classes.SideDrawer,classes.Close];
+    if(props.open){
+        attachedClass=[classes.SideDrawer,classes.Open];
+    }
     return (
-        <div className={classes.SideDrawer}>
-            <div className={classes.Logo}>
+        <Aux>
+            <Backdrop show={props.open} clicked={props.closed}/>
+            <div className={attachedClass.join(" ")}>
+                <div className={classes.Logo}>
 
-                <Logo/>
+                    <Logo/>
+                </div>
+
+                <nav>
+                    <NavigationItems />
+                </nav>
             </div>
-
-            <nav>
-                <NavigationItems />
-            </nav>
-        </div>
+        </Aux>
+        
     )
 }
 
